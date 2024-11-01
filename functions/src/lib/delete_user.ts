122a5e3d5d5d924ed8aa16fc489ed8c1eef6b005
shuya729 <sshuya29@gmail.com>
2024-11-01 22:28:03 +0900

    1.0.2

--- functions/src/lib/delete_user.ts
+++ functions/src/lib/delete_user.ts
@@ -1,27 +1,29 @@
 import { deleteCollection } from "../utils/delete_collections";
-import { user } from "firebase-functions/v1/auth";
+import { region } from "firebase-functions/v1";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
 
-export const deleteUser = user().onDelete(async (user) => {
-  const authId = user.uid;
-  const db = getFirestore();
-  const storage = getStorage();
+export const deleteUser = region("asia-northeast1")
+  .auth.user()
+  .onDelete(async (user) => {
+    const authId = user.uid;
+    const db = getFirestore();
+    const storage = getStorage();
 
-  const userRef = db.collection("users").doc(authId);
-  await userRef.delete();
+    const userRef = db.collection("users").doc(authId);
+    await userRef.delete();
 
-  const infoRef = db.collection("infos").doc(authId);
-  await infoRef.delete();
+    const infoRef = db.collection("infos").doc(authId);
+    await infoRef.delete();
 
-  const bucket = storage.bucket();
-  const userBucket = bucket.file(`users/${authId}/icon.jpg`);
-  const [bucketExists] = await userBucket.exists();
-  if (bucketExists) await userBucket.delete();
+    const bucket = storage.bucket();
+    const userBucket = bucket.file(`users/${authId}/icon.jpg`);
+    const [bucketExists] = await userBucket.exists();
+    if (bucketExists) await userBucket.delete();
 
-  const batchSize = 100;
-  const subCollections = await userRef.listCollections();
-  for (const subCollection of subCollections) {
-    await deleteCollection(db, subCollection, batchSize);
-  }
-});
+    const batchSize = 100;
+    const subCollections = await userRef.listCollections();
+    for (const subCollection of subCollections) {
+      await deleteCollection(db, subCollection, batchSize);
+    }
+  });
--- functions/src/lib/user_crawler.ts
+++ functions/src/lib/user_crawler.ts
@@ -1,4 +1,4 @@
-import { getFirestore } from "firebase-admin/firestore";
+import { getFirestore, Timestamp } from "firebase-admin/firestore";
 import { onSchedule } from "firebase-functions/v2/scheduler";
 import { UserInfo } from "../models/user_info";
 import { getAuth } from "firebase-admin/auth";
@@ -8,23 +8,26 @@ export const userCrawler = onSchedule(
     schedule: "every day 03:00",
     timeZone: "Asia/Tokyo",
     region: "asia-northeast1",
+    timeoutSeconds: 300,
   },
   async () => {
     const now = new Date();
     const baseTime = 24 * 60 * 60 * 1000;
+    const baseStamp = Timestamp.fromMillis(now.getTime() - baseTime);
     const db = getFirestore();
     const auth = getAuth();
 
     const query = db
       .collection("infos")
       .where("anonymousFlg", "==", true)
-      .where("lastAt", "<", now.getTime() - baseTime);
+      .where("lastAt", "<", baseStamp);
     const snapshot = await query.get();
     if (snapshot.empty) return;
 
     const authIds: string[] = [];
-    snapshot.docs.forEach((doc) => {
-      authIds.push(new UserInfo(doc.data()).authId);
+    snapshot.forEach((doc) => {
+      const data = doc.data();
+      authIds.push(new UserInfo(data).authId);
     });
 
     for (const authId of authIds) {
